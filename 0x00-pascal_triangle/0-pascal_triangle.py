0c8e00eef23539077468ae08bae9ac92ded67128
Shumani <shumanisimeli@gmail.com>
2024-06-30 23:57:20 +0200

    first commit
    mend

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,22 @@
-for n in range(5):
-    row = [1] * (n + 1)
-    print(n, end=' ')
-    for k in range(1,n):
-        print(k)
+def pascal_triangle(n):
+    triangle_list = []
+    if n <= 0:
+        return triangle_list
+    
+    for i in range(n):
+        row_list = [1] * (i + 1)
+        for j in range(1, i):
+            row_list[j] = triangle_list[i - 1][j - 1] + triangle_list[i - 1][j]
+        triangle_list.append(row_list)
+    return triangle_list
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
