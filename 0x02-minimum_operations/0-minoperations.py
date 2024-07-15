4cded0648ae6efcc3e2ad95ff3b5adfb503fa272
shumani <shumanisimeli@gmail.com>
2024-07-15 04:18:02 +0200

    first commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while factor * factor <= n:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    if n > 1:
+        operations += n
+
+    return operations
