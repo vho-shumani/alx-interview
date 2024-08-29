5cf9f4c5fa4c24061b0a2dde25a8617447521e0a
shumani <shumanisimeli@gmail.com>
2024-08-29 22:16:34 +0200

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -21,9 +21,8 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Start with 4 sides for each land cell
+                perimeter += 4  
 
-                # Check adjacent cells and subtract shared sides
                 if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j-1] == 1:
