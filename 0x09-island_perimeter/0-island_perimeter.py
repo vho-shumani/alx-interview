6a6c9789c180f3da687cc3154046ecd748381a47
shumani <shumanisimeli@gmail.com>
2024-08-29 22:18:57 +0200

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -21,8 +21,7 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  
-
+                perimeter += 4
                 if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j-1] == 1:
