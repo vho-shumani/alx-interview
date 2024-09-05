064194919adfb3da934f3a63fee70ee4c547c17d
shumani <shumanisimeli@gmail.com>
2024-09-05 20:39:12 +0200

    fixed pycodestyle errors

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """0-prime_game.py"""
+
+
 def isWinner(x, nums):
     """Return: name of the player that won the most rounds"""
-    
+
     def sieve_of_eratosthenes(n):
         primes = [True] * (n + 1)
         primes[0] = primes[1] = False
@@ -11,21 +13,21 @@ def isWinner(x, nums):
                 for j in range(i * i, n + 1, i):
                     primes[j] = False
         return primes
-    
+
     max_n = max(nums)
     primes = sieve_of_eratosthenes(max_n)
-    
+
     def play_game(n):
         remaining = set(range(1, n + 1))
         moves = 0
-        
+
         for p in range(2, n + 1):
             if primes[p] and p in remaining:
                 moves += 1
                 remaining -= set(range(p, n + 1, p))
-        
+
         return "Maria" if moves % 2 == 1 else "Ben"
-    
+
     maria_wins = ben_wins = 0
     for n in nums:
         winner = play_game(n)
@@ -40,4 +42,3 @@ def isWinner(x, nums):
         return "Ben"
     else:
         return None
-
