1c1adada943877aade5da2e3ceb74207d4d8385a
shumani <shumanisimeli@gmail.com>
2024-10-04 04:42:01 +0200

    first commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""Module contain makechange function"""
+
+
 def makeChange(coins, total):
+    """Determine the fewest number of coins needed to meet a given amount total."""
     if total <= 0:
         return 0
     
